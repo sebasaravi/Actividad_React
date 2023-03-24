b1743e8ef5f5dd916192d27710bc3bfc80b62919
sebasaravi <sebastian.arias.aviles@est.una.ac.cr>
2023-03-24 16:23:15 -0600

    Mejorando nuestro repositorio

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HeroSection from './components/HeroSection'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+/*module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}*/
+module.exports = { content:["./src/**/*.{ts,tsx}"],
+ theme: { extend: {}, }, plugins: [], }
